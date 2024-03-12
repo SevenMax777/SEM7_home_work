102de7fbcd3f38d5bcf40197c60b93fb5eb56060
Max <seven.85@list.ru>
2024-03-12 16:32:07 +0500

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int Ackermann(int m, int n)
+{
+	if (m == 0)
+	{
+		return n + 1;
+	}
+	else if (n == 0)
+	{
+		return Ackermann(m - 1, 1);
+	}
+	else
+	{
+		return Ackermann(m - 1, Ackermann(m, n - 1));
+	}
+}
+
+int m = 2;
+int n = 3;
+int result = Ackermann(m, n);
+Console.WriteLine($"A({m},{n}) = {result}");
