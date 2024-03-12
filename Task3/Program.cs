f1cb6f77ab04da238d70f30a2be1cd9ea4d2e0bb
Max <seven.85@list.ru>
2024-03-12 16:50:57 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿void PrintArrayReverse(int[] array, int i)
+{
+	if (i < 0)
+		return;
+
+	Console.Write(array[i] + " ");
+	PrintArrayReverse(array, i - 1);
+}
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+PrintArrayReverse(array, array.Length - 1);
